1fade84b55889bb0c2df19b4e08ce26db2d65969
fabiooo4 <64950091+fabiooo4@users.noreply.github.com>
2022-04-06 12:34:20 +0200

    Add files via upload

---
+++ src/com/irimie/Client.java
@@ -0,0 +1,57 @@
+package com.irimie;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+// Client class
+public class Client
+{
+    public static void main(String[] args) throws IOException
+    {
+        try
+        {
+            Scanner scn = new Scanner(System.in);
+
+            // getting localhost ip
+            InetAddress ip = InetAddress.getByName("10.4.54.14");
+
+            // establish the connection with server port 5056
+            Socket s = new Socket(ip, 3012);
+
+            // obtaining input and out streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+            // the following loop performs the exchange of
+            // information between client and client handler
+            while (true)
+            {
+                System.out.println(dis.readUTF());
+                String tosend = scn.nextLine();
+                dos.writeUTF(tosend);
+
+                // If client sends exit,close this connection
+                // and then break from the while loop
+                if(tosend.equals("Exit"))
+                {
+                    System.out.println("Chiudendo la connessione : " + s);
+                    s.close();
+                    System.out.println("Connessione chiusa");
+                    break;
+                }
+
+                // printing date or time as requested by client
+                String received = dis.readUTF();
+                System.out.println(received);
+            }
+
+            // closing resources
+            scn.close();
+            dis.close();
+            dos.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
